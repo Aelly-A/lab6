acfc42cf9d645034bbb311aacebbb94026f69f7c
Aelly Alwardi <aellyalwardi@endpoint-10-192-158-231.ufvisitor.ufl.edu>
2023-03-07 15:41:51 -0500

    first commit

---
+++ encoder.py
@@ -0,0 +1,32 @@
+def main():
+    choice = '0'
+    while choice != '3':
+        print_menu()
+        choice = input("Please enter an option: ")
+        number = input("Please enter your password to encode: 12345555")
+
+        if choice == '1':
+            result = encode(number)
+            print("Your password has been encoded and stored!\n")
+
+
+def print_menu():
+    print("Menu")
+    print("-------------")
+    print('1. Encoder')
+    print('2. Decoder')
+    print('3. Exit')
+    print()
+
+
+def encode(number):
+    encoded_number = ""
+    for digit in str(number):
+        digit = int(digit)
+        encoded_number += str(digit + 3 if digit < 7 else digit - 7)
+
+    return int(encoded_number)
+
+
+if __name__ == '__main__':
+    main()
